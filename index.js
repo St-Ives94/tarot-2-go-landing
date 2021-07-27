c6e2c98790e1d443f307dbf5ea30fe529d8ebb15
Ivory Smith <smith.ivory94@gmail.com>
2021-07-27 09:20:41 -0500

    updating

---
+++ index.js
@@ -0,0 +1,8 @@
+const title = document.getElementById('backsplash');
+
+const transitionOut = () => {
+    document.getElementById('title').style.transition = 'all 2s ease-in ';
+        // console.log('dom has loaded')
+
+}
+transitionOut();
